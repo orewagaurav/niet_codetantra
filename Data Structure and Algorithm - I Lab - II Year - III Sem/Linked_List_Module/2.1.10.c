d54461c6e0c9bd6bc96fa714695edf840f78b375
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-10-27 13:09:55 +0530

    DSA

--- Data Structure and Algorithm - I Lab - II Year - III Sem/Linked_List_Module/2.1.10.c
+++ Data Structure and Algorithm - I Lab - II Year - III Sem/Linked_List_Module/2.1.10.c
@@ -50,5 +50,4 @@ int main(int argc, char *argv[]) {
 	int resultsArrLength = mergeTwoSLL(list1, list1Len, list2, list2Len, resultsArr);
 	printArrayElements(resultsArr, resultsArrLength);
 	return 0;
-	
 }
