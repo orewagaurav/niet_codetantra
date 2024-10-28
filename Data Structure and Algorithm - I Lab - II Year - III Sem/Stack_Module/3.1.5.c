c575556475ac41ae186e8708ac299cc49e5b6969
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-10-29 00:32:40 +0530

    DSA

---
+++ Data Structure and Algorithm - I Lab - II Year - III Sem/Stack_Module/3.1.5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+	int n;
+	scanf("%d",&n);
+	int arr[n];
+	for(int i= 0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	for(int i=n-1;i>=0;i--){
+		if(i>0)
+		printf("%d ",arr[i]);
+		else
+			printf("%d",arr[i]);
+	}
+	
+}
