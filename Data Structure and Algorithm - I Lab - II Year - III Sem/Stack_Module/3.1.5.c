176c962cdf37e337563031748e638b0330289295
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-10-30 23:05:33 +0530

    DSA

--- Data Structure and Algorithm - I Lab - II Year - III Sem/Stack_Module/3.1.5.c
+++ Data Structure and Algorithm - I Lab - II Year - III Sem/Stack_Module/3.1.5.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+struct node{
+	int data;
+	struct node *next;
+}*top = NULL;
+
+void push(int value){
+	struct node *ptr;
+	ptr = malloc(sizeof(struct node));
+	ptr->data = value;
+	ptr->next = top;
+	top=ptr;
+}
+void display(){
+	struct node* tmp;
+	if(top==NULL)
+	printf("stack is empty\n");
+	tmp=top;
+	while(tmp->next!=NULL){
+		printf("%d ",tmp->data);
+		tmp=tmp->next;
+	}
+	printf("%d\n",tmp->data);
+}
 int main(){
-	int n;
+	int n,value;
 	scanf("%d",&n);
-	int arr[n];
-	for(int i= 0;i<n;i++){
-		scanf("%d",&arr[i]);
+	for(int i=0;i<n;i++){
+		scanf("%d",&value);
+		push(value);
 	}
-	for(int i=n-1;i>=0;i--){
-		if(i>0)
-		printf("%d ",arr[i]);
-		else
-			printf("%d",arr[i]);
-	}
-	
+	display();
 }
