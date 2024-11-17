6c8c4fa27204a72a8023a4d7235a5e6f292e31de
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-11-17 21:37:35 +0530

    DSA

---
+++ Data Structure and Algorithm - I Lab - II Year - III Sem/Greedy_Algorithim/5.1.1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *a,int *b){
+	int temp =*a;
+	*a = *b;
+	*b =temp;
+}
+void sort(int values[],int weight[],int n){
+	double ratio[n];
+	for(int i=0;i<n;i++){
+		ratio[i]=(double)values[i]/weight[i];
+	}
+	for(int i=0;i<n-1;i++){
+		for(int j=i+1;j<n;j++){
+			if(ratio[i]<ratio[j]){
+				double tmp = ratio[i];
+				ratio[i]=ratio[j];
+				ratio[j]=tmp;
+
+				swap(&values[i],&values[j]);
+				swap(&weight[i],&weight[j]);
+			}
+		}
+	}
+}
+double max(int values[],int weight[],int n,int capacity){
+	sort(values,weight,n);
+	double totalVal = 0.0;
+	for(int i=0;i<n&&capacity>0;i++){
+		if(weight[i]<=capacity){
+			capacity -=weight[i];
+			totalVal +=values[i];
+		}
+		else{
+			totalVal += (double)values[i]*capacity/weight[i];
+			break;
+		}
+	}
+	return totalVal;
+}
+int main(){
+	int n,capacity;
+	scanf("%d %d",&n,&capacity);
+	int values[n],weight[n];
+	for(int i=0;i<n;i++){
+		scanf("%d %d",&values[i],&weight[i]);
+	}
+	double MAX = max(values,weight,n,capacity);
+	printf("%.6f\n",MAX);
+	return 0;
+}
