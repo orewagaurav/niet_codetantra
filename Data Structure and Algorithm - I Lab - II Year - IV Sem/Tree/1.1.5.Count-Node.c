92bb4e5726d209c404fc982edc3a0919c4549b56
orewagaurav <gauravkumar47967@gmail.com>
2025-02-14 23:37:59 +0530

    tree

---
+++ Data Structure and Algorithm - I Lab - II Year - IV Sem/Tree/1.1.5.Count-Node.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+	int data;
+	struct node *left;
+	struct node *right;
+};
+
+struct node *create(){
+	int data;
+	struct node * new;
+	new=(struct node*)malloc(sizeof(struct node));
+	scanf("%d",&data);
+	if(data==-1){
+		return 0;
+	}
+	new->data=data;
+	new->left=create();
+	new->right=create();
+	return new;
+}
+int count(struct node *root){
+	if(root==NULL){
+		return 0;
+	}
+	return count(root->left)+count(root->right)+1;
+}
+int main(){
+	struct node *root;
+	root =create();
+	printf("%d",count(root));
+}
